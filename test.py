09160848db91ff166048723717e112fbd482eca8
Mohamed Saad <m1hamed98@gmail.com>
2021-06-20 12:43:33 +0200

    adding unit test

---
+++ test.py
@@ -0,0 +1,20 @@
+import unittest
+from project import app
+
+class ProjectTests(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+        self.assertEquals(app.debug, False)
+    
+    def tearDown(self):
+        pass
+
+    def test_login_page(self):
+        res = self.app.get('/', follow_redirects=True)
+        self.assertIn(b'LOGIN', res.data)
+
+if __name__ == "__main__":
+    unittest.main()
